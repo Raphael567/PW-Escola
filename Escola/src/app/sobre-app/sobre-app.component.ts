be11503fce057b1ec7263f4261eda0bb272704e9
coletti <matheus.coletti08@gmail.com>
2025-06-10 10:38:02 -0300

    arquivos iniciais

---
+++ Escola/src/app/sobre-app/sobre-app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sobre-app',
+  imports: [],
+  templateUrl: './sobre-app.component.html',
+  styleUrl: './sobre-app.component.css'
+})
+export class SobreAppComponent {
+
+}
