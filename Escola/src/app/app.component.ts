20eb148c1234e673008a3d48a4a3f0b0dd6667d5
Raphael567 <raphaelcanuto464@gmail.com>
2025-06-13 15:22:53 -0300

    Feat: arquivos inciais :sparkles:

--- Escola/src/app/app.component.ts
+++ Escola/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ContatoComponent } from './contato/contato.component';
+import { SobreComponent } from './sobre/sobre.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, LoginComponent, SobreComponent, ContatoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
